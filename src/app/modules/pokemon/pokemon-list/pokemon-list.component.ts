88231cb5d32d5d3022164595f7f632c1eb48bdd2
byronius brownius iv <you@example.com>
2022-07-13 13:52:07 -0400

    populated pkmn list

--- src/app/modules/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/modules/pokemon/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PokemonList } from '../models';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -7,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PokemonListComponent implements OnInit {
 
-  @Input() pokemonList: any;
+  @Input() pokemonList!: PokemonList | null;
 
   selectedPokemon: any;
 
