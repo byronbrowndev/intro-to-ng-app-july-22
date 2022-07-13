b389d142afd8fdbfdfdd6220e0246f6b6da1cdc8
byronius brownius iv <you@example.com>
2022-07-13 15:00:40 -0400

    pokemonDetail req complete

--- src/app/modules/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/modules/pokemon/pokemon-list/pokemon-list.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { PokemonList } from '../models';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PokemonList, PokemonListItem } from '../models';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent {
 
   @Input() pokemonList!: PokemonList | null;
+  @Output() emitSelectedPokemon: EventEmitter<PokemonListItem> = new EventEmitter();
 
   selectedPokemon: any;
 
   constructor() { }
 
-  ngOnInit(): void {
+  pokemonSelected(pokemon: PokemonListItem): void {
+    this.selectedPokemon = pokemon;
+    console.log(pokemon);
+    this.emitSelectedPokemon.emit(pokemon);
   }
 
 }
--- src/app/modules/pokemon/pokemon/pokemon.component.ts
+++ src/app/modules/pokemon/pokemon/pokemon.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { PokemonDetail, PokemonList } from '../models';
+import { PokemonDetail, PokemonList, PokemonListItem } from '../models';
 import { PokemonService } from '../services/pokemon.service';
 
 @Component({
@@ -12,17 +12,8 @@ export class PokemonComponent implements OnInit {
 
 
   // app-pokemon-detail
-  // fake data that returns fromt he call to pokemon
-  selectedPokemon: PokemonDetail | undefined =
-    {
-      id: '1',
-      name: 'bulbasaur',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-      type: 'grass/poison',
-      height: 0,
-      weight: 0
-    }
 
+  selectedPokemon$: Observable<any> | undefined;
   pokemonList$!: Observable<PokemonList>;
 
   pokemonTeamMember: PokemonDetail | null = {
@@ -40,6 +31,15 @@ export class PokemonComponent implements OnInit {
   ngOnInit(): void {
     // call out to a service that calls out to the api and fetches a value
     this.pokemonList$ = this.pokemonService.getPokemonList();
+  }
+
+  requestPokemonDetail(pokemonListItem: PokemonListItem): void {
+    // take the url
+    // console.log(pokemonListItem)
+    this.selectedPokemon$ = this.pokemonService.getPokemonDetail(pokemonListItem.url);
+    // make a call to the url using the srvice
+    // get resp back as obs
+    // pass that to detail using prop binding and the async pip
   }
 
 }
